b19c8d43fa9b692250c8105ffa832234bce8b1cf
Junior Mayta <junior.mayta@filewave.com>
2024-03-15 13:34:54 +0100

    Removing cronjobs when project start

--- deployment/scripts/backend/start.sh
+++ deployment/scripts/backend/start.sh
@@ -3,6 +3,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py migrate --noinput
-service cron start
-python manage.py crontab add
 python manage.py runserver "$APP_HOST":"$APP_PORT"
